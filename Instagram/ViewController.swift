2d395a43f1f85fa33ed642ebfad5189a1e5f0abe
fumiya.motoyoshi <fumiya.motoyoshi0238@gmail.com>
2020-02-26 17:17:23 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Fumiya Motoyoshi on 2020/02/26.
+//  Copyright © 2020 fumiya.motoyoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
