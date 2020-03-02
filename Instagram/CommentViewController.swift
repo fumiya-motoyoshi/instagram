9aac4c9204076e33c35c50b473c0a79829ea155a
fumiya.motoyoshi <fumiya.motoyoshi0238@gmail.com>
2020-03-02 10:49:18 +0900

    カリキュラムの内容にコメント用ViewControllerを追加

---
+++ Instagram/CommentViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CommentViewController.swift
+//  Instagram
+//
+//  Created by Fumiya Motoyoshi on 2020/03/02.
+//  Copyright © 2020 fumiya.motoyoshi. All rights reserved.
+//
+
+import Foundation
---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Fumiya Motoyoshi on 2020/03/01.
+//  Copyright © 2020 fumiya.motoyoshi. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
